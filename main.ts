e5fd25f9d1331971ea4ae915bf8c4d2cc7b2d25d
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2024-04-11 14:23:56 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
     Fentes += 1
 })
-let t2 = 0
 let Vitesse = 0
-let t1 = 0
 let Fentes = 0
 pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 let d1 = 0
 let d2 = 0
+let t1 = 0
+let t2 = 0
 basic.forever(function () {
     t1 = input.runningTime() / 1000
     // Le nombre de fentes comptes / le nombre de fentes dans la roue avec fentes * la circonférence de la roue en cm / 100 pour mettre la mesure en mètre.
