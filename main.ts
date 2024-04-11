0551e77d0a2fda51869d68a50f46d7c2ba3917cf
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2024-04-11 14:23:21 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,22 @@
 pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
     Fentes += 1
 })
+let t2 = 0
+let Vitesse = 0
+let t1 = 0
 let Fentes = 0
 pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
-let f1 = 0
-let f2 = 0
+let d1 = 0
+let d2 = 0
 basic.forever(function () {
-    f1 = Fentes
-    if (f1 > f2) {
-    	
-    }
-    f2 = f1
-    basic.pause(50)
+    t1 = input.runningTime() / 1000
+    // Le nombre de fentes comptes / le nombre de fentes dans la roue avec fentes * la circonférence de la roue en cm / 100 pour mettre la mesure en mètre.
+    d1 = Fentes / 6 * 9.56 / 100
+    // Calcul de la vitesse instantanée
+    Vitesse = Math.abs(d2 - d1) / Math.abs(t2 - t1)
+    serial.writeValue("Vitesse", Vitesse)
+    t2 = t1
+    d2 = d1
+    led.toggle(0, 0)
+    basic.pause(100)
 })
